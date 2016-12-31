2d318bbda393776e560ce3be90fe0256e9ef08bf
nongsang <shdtkd37@gmail.com>
2017-01-01 01:18:50 +0900

    1월 1일 배열 포인터 응용

---
+++ 08.array_pointer_apply.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[3] = { 10, 20, 30 };
+	int (*ap) = NULL;
+	
+	ap = arr;
+
+	for (int i = 0; i < 3; ++i)
+		printf("arr[%d] = %d\n",i, arr[i]);
+	printf("\n");
+
+	for (int i = 0; i < 3; ++i)
+		printf("ap[%d] = %d\n", i, ap[i]);
+}
+// 8번째 줄
+// 포인터 변수에 배열을 저장
+// int* arr1[3]을 만들어서 
+
+// 14, 15번째 줄
+// 포인터 변수가 배열을 대신하여 값을 출력해준다.
